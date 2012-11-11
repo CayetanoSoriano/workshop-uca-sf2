831aff45ee539906e56d4625da1bdee5c992a655
Cayetano <neoshadybeat@gmail.com>
2012-11-11 05:06:44 +0100

    Added doctrine Entity Post

--- src/workshop/BlogBundle/Controller/DefaultController.php
+++ src/workshop/BlogBundle/Controller/DefaultController.php
@@ -5,15 +5,30 @@ namespace workshop\BlogBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use workshop\BlogBundle\Entity\Post;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/hello/{name}/{poc}")
+     * @Route("/hello/{name}/")
      * @Template()
      */
     public function indexAction($name)
     {
-        return array('name' => $name);
+
+    	$em = $this->getDoctrine()->getEntityManager();
+
+    	$post = new Post();
+    	$post->setTitle('hola')
+    	->setText('que tal')
+    	->setSlug($post->getTitle())
+    	->setDate(new \DateTime())
+    	;
+
+    	$em->persist($post);
+    	$em->flush();
+
+    	$posts = $em->getRepository('workshopBlogBundle:Post')->findAll();
+    	return array('posts' => $posts);
     }
 }
